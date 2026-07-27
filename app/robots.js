5d9b756404dcf93379be02b309dddc5da0143317
edy2209 <edisyafrianto45@gmail.com>
2026-07-27 19:52:18 +0700

    feat: add sitemap.xml and robots.txt for Google Search Console SEO

---
+++ app/robots.js
@@ -0,0 +1,14 @@
+// Robots.txt otomatis Next.js App Router
+// Akses: https://rtnewssumbar.my.id/robots.txt
+
+export default function robots() {
+    return {
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+        ],
+        sitemap: 'https://rtnewssumbar.my.id/sitemap.xml',
+    };
+}
